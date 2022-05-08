ba8f721761fb14fcfaebf607ca17ff081b99fa51
Ivan Onishchenko <Lightsout07@mail.ru>
2022-05-08 11:34:53 +0300

    pret

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import readlineSync from "readline-sync";
 
 import { userName, nameUser } from "../src/cli.js";
 
@@ -8,24 +8,23 @@ console.log("Welcome to the Brain Games!");
 userName();
 
 const gameEvenOdd = (youranswer) => {
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-    let count = 3;
-    for (let i = 0; i < count; i++) {
-        let number = Math.round(Math.random() * 100);
-        console.log(`Question: ${number}`);
-        let answer = readlineSync.question('Your answer: ');
-        if (answer === 'yes' && (number % 2) === 0) {
-            console.log('Correct!');
-        } else if(answer === 'no' && (number % 2) !== 0) {
-            console.log('Correct!');
-        } else {
-            return console.log(`Let's try again, ${nameUser}!`)
-        }
-        if (i === 2) {
-            return console.log(`Congratulations, ${nameUser}!`);
-        }
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  let count = 3;
+  for (let i = 0; i < count; i++) {
+    let number = Math.round(Math.random() * 100);
+    console.log(`Question: ${number}`);
+    let answer = readlineSync.question("Your answer: ");
+    if (answer === "yes" && number % 2 === 0) {
+      console.log("Correct!");
+    } else if (answer === "no" && number % 2 !== 0) {
+      console.log("Correct!");
+    } else {
+      return console.log(`Let's try again, ${nameUser}!`);
     }
-}
-
+    if (i === 2) {
+      return console.log(`Congratulations, ${nameUser}!`);
+    }
+  }
+};
 
 gameEvenOdd();
